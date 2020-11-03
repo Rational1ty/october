8f6ce7763e098ef6bc66bf5d9bdb9be6794f9ffd
Matthew Davidson <mdavidson390@outlook.com>
2020-11-03 09:55:19 -0600

    Minor styling changes and added canvas init to background.js

--- scripts/background.js
+++ scripts/background.js
@@ -1,3 +1,6 @@
+let canvas;
+let ctx;
+
 function $(id) {
 	return document.getElementById(id);
 }
@@ -7,14 +10,28 @@ function $(id) {
 // });
 
 window.addEventListener('load', () => {
+	// Set viewport scroll to top- and left-most position
 	if ('scrollRestoration' in window.history) {
 		window.history.scrollRestoration = 'manual';
 	}
 	window.scroll(0, 0);
+
+	// TODO: make this use previous setting or user setting
 	$('music').volume = 0.2;
+
+	// Initialize canvas and drawing context
+	canvas = $('audio-slider');
+	ctx = canvas.getContext('2d');
 });
 
 $('audio-toggle').addEventListener('click', e => toggleAudio(e.target));
+
+$('volume-control').addEventListener('mouseenter', () => {
+	$('audio-slider').style.setProperty('display', 'initial');
+});
+$('volume-control').addEventListener('mouseleave', () => {
+	$('audio-slider').style.setProperty('display', 'none');
+});
 
 function toggleAudio(element) {
 	if (element.getAttribute('src') === './assets/music-note.png') {
@@ -25,11 +42,3 @@ function toggleAudio(element) {
 		$('music').play();
 	}
 }
-
-$('volume-control').addEventListener('mouseenter', () => {
-	$('audio-slider').style.setProperty('display', 'initial');
-});
-
-$('volume-control').addEventListener('mouseleave', () => {
-	$('audio-slider').style.setProperty('display', 'none');
-});
