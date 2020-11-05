8055a64be7641d013d84f324b4bd5363ef1575e5
Matthew Davidson <mdavidson390@outlook.com>
2020-11-05 09:48:51 -0600

    Volume slider now opens/closes on hover; changed default opacity of bg from 10% -> 25%

--- scripts/background.js
+++ scripts/background.js
@@ -5,10 +5,6 @@ function $(id) {
 	return document.getElementById(id);
 }
 
-// window.addEventListener('resize', () => {
-// 	console.log(window.visualViewport.width, window.visualViewport.height);
-// });
-
 window.addEventListener('load', () => {
 	// Set viewport scroll to top- and left-most position
 	if ('scrollRestoration' in window.history) {
@@ -20,18 +16,10 @@ window.addEventListener('load', () => {
 	$('music').volume = 0.2;
 
 	// Initialize canvas and drawing context
-	canvas = $('audio-slider');
-	ctx = canvas.getContext('2d');
+	ctx = $('audio-slider').getContext('2d');
 });
 
 $('audio-toggle').addEventListener('click', e => toggleAudio(e.target));
-
-$('volume-control').addEventListener('mouseenter', () => {
-	$('audio-slider').style.setProperty('display', 'initial');
-});
-$('volume-control').addEventListener('mouseleave', () => {
-	$('audio-slider').style.setProperty('display', 'none');
-});
 
 function toggleAudio(element) {
 	if (element.getAttribute('src') === './assets/music-note.png') {
@@ -41,4 +29,25 @@ function toggleAudio(element) {
 		element.setAttribute('src', './assets/music-note.png');
 		$('music').play();
 	}
+}
+
+const volumeControl = $('volume-control');
+const audioSlider = $('audio-slider');
+
+volumeControl.addEventListener('mouseenter', () => {
+	audioSlider.style.setProperty('display', 'initial');
+});
+volumeControl.addEventListener('mouseleave', () => {
+	audioSlider.style.setProperty('display', 'none');
+});
+
+audioSlider.addEventListener('mousedown', () => {
+	audioSlider.onmousemove = e => adjustVolume(e);
+});
+audioSlider.addEventListener('mouseup', () => {
+	audioSlider.onmousemove = null;
+});
+
+function adjustVolume(e) {
+	console.log(e.offsetX);
 }
